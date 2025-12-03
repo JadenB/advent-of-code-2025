310e72edabc5fafe9f5556edaae33d9d832a72f9
JadenB <jadenbbernal@gmail.com>
2025-12-02 18:31:32 -0800

    Day 1 Part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,9 +2,14 @@ use std::fs;
 
 const DIAL_SIZE: i32 = 100;
 
+fn positive_modulo(n: i32, m: i32) -> i32 {
+    ((n % m) + m) % m
+}
+
 fn main() {
     let input = fs::read_to_string("input/input.txt").unwrap();
 
+    // Part 1
     let mut password = 0;
     let mut dial = 50;
     for line in input.lines() {
@@ -16,11 +21,27 @@ fn main() {
             magnitude
         };
 
-        dial = (dial + DIAL_SIZE + rotation) % DIAL_SIZE;
+        dial = positive_modulo(dial + rotation, DIAL_SIZE);
         if dial == 0 {
             password += 1;
         }
     }
+    println!("{password}");
 
+    // Part 2
+    let mut password = 0;
+    let mut dial = 50;
+    for line in input.lines() {
+        let magnitude: i32 = line[1..].parse().unwrap();
+
+        let (rotation, distance_past_zero) = if line.starts_with("L") {
+            (-magnitude, (DIAL_SIZE - dial) % DIAL_SIZE)
+        } else {
+            (magnitude, dial)
+        };
+
+        password += (distance_past_zero + magnitude) / DIAL_SIZE;
+        dial = positive_modulo(dial + rotation, DIAL_SIZE);
+    }
     println!("{password}");
 }
